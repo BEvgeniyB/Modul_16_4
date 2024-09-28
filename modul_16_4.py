3e137b046c5ef3b0ab5cda50d28d8aa89399711f
BEvgeniyB <masterbe2012@yandex.ru>
2024-09-28 09:33:03 +0300

    Update modul_16_4.py

--- modul_16_4.py
+++ modul_16_4.py
@@ -52,7 +52,7 @@ async def user_update(user_id: Annotated[ int,Path(ge=1, le=100, description="En
         user.age = age
         return f"User {user} has been updated"
     except IndexError:
-        raise HTTPException(status_code=404,detail="Index not found")
+        raise HTTPException(status_code=404,detail="User was not found")
 
 
 
@@ -63,4 +63,4 @@ async def delete_user(user_id: int = Path(ge=1, le=100, description="Enter User 
         users.remove(user)
         return f"User {user} has been deleted"
     except IndexError:
-        raise HTTPException(status_code=404, detail="Index not found")
+        raise HTTPException(status_code=404, detail="User was not found")
